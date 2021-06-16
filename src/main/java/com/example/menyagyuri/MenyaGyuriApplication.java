da85919f64fcf5156d4ab0722285ac44c239fef7
vargagyorgy93 <vargagyorgy93@gmail.com>
2021-06-16 18:12:16 +0200

    first

---
+++ src/main/java/com/example/menyagyuri/MenyaGyuriApplication.java
@@ -0,0 +1,13 @@
+package com.example.menyagyuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenyaGyuriApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MenyaGyuriApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/menyagyuri/MenyaGyuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menyagyuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenyaGyuriApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
